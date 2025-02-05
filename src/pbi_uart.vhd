3f3d95a1202ea459e89ea0a6869236c856f94f98
deuskane <deuskane@gmail.com>
2025-02-05 07:04:20 +0100

    uart rx : add fsm and use tick half

--- src/pbi_uart.vhd
+++ src/pbi_uart.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosiere
 -- Company    : 
 -- Created    : 2025-01-21
--- Last update: 2025-01-29
+-- Last update: 2025-02-05
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -30,6 +30,8 @@ entity pbi_UART is
     BAUD_RATE           : integer := 115200;
     CLOCK_FREQ          : integer := 50000000;
     BAUD_TICK_CNT_WIDTH : integer := 16;
+    UART_TX_ENABLE      : boolean := true;
+    UART_RX_ENABLE      : boolean := true;
     ID                  : std_logic_vector (PBI_ADDR_WIDTH-1 downto 0) := (others => '0')
     );
   port   (
@@ -50,22 +52,44 @@ end entity pbi_UART;
 architecture rtl of pbi_UART is
   constant SIZE_ADDR_IP : natural := 2;
   
-  signal ip_cs               : std_logic;
-  signal ip_re               : std_logic;
-  signal ip_we               : std_logic;
-  signal ip_addr             : std_logic_vector (SIZE_ADDR_IP-1   downto 0);
-  signal ip_wdata            : std_logic_vector (PBI_DATA_WIDTH-1 downto 0);
-  signal ip_rdata            : std_logic_vector (PBI_DATA_WIDTH-1 downto 0);
-  signal ip_busy             : std_logic;
+  signal ip_cs                  : std_logic;
+  signal ip_re                  : std_logic;
+  signal ip_we                  : std_logic;
+  signal ip_addr                : std_logic_vector (SIZE_ADDR_IP-1   downto 0);
+  signal ip_wdata               : std_logic_vector (PBI_DATA_WIDTH-1 downto 0);
+  signal ip_rdata               : std_logic_vector (PBI_DATA_WIDTH-1 downto 0);
+  signal ip_busy                : std_logic;
+                            
+  signal uart_tx                : std_logic;
+  signal uart_rx                : std_logic;
 
-  signal uart_baud_tick      : std_logic;
+  signal uart_tx_baud_tick_en   : std_logic;
+  signal uart_tx_baud_tick      : std_logic;
 
-  signal s_axis_tvalid       : std_logic;
-  signal s_axis_tready       : std_logic;
-  signal s_axis_tdata        : std_logic_vector(8-1 downto 0);
+  signal uart_rx_baud_tick_en   : std_logic;
+  signal uart_rx_baud_tick      : std_logic;
+  signal uart_rx_baud_tick_half : std_logic;
 
+  signal s_axis_tvalid          : std_logic;
+  signal s_axis_tready          : std_logic;
+  signal s_axis_tdata           : std_logic_vector(8-1 downto 0);
+                         
+  signal m_axis_tvalid          : std_logic;
+  signal m_axis_tready          : std_logic;
+  signal m_axis_tdata           : std_logic_vector(8-1 downto 0);
+
+  signal parity_enable          : std_logic;
+  signal parity_odd             : std_logic;
+  signal uart_loopback          : std_logic;
+  
 begin  -- architecture rtl
-    
+
+  parity_enable        <= '0';
+  parity_odd           <= '0';
+  uart_loopback        <= '1';
+
+  uart_tx_o            <= uart_tx;
+  
   ins_pbi_wrapper_target : entity work.pbi_wrapper_target(rtl)
   generic map(
     SIZE_DATA      => PBI_DATA_WIDTH,
@@ -87,54 +111,102 @@ begin  -- architecture rtl
     pbi_tgt_o      => pbi_tgt_o     
     );
 
-  ins_uart_baud_rate_gen : entity work.uart_baud_rate_gen(rtl)
-    generic map(
-      BAUD_RATE      => BAUD_RATE,
-      CLOCK_FREQ     => CLOCK_FREQ
-      )
-    port map(
-      clk_i            => clk_i   ,
-      arst_b_i         => arst_b_i,
-      baud_tick_en_i   => '1',
-      baud_tick_o      => uart_baud_tick,
-      baud_tick_half_o => open
-      );
+  ip_rdata             <= m_axis_tdata;
+  ip_busy              <= not s_axis_tready;
 
-  s_axis_tvalid <= ip_cs and ip_we;
-  s_axis_tdata  <= ip_wdata;
-  ip_busy       <= not s_axis_tready;
-  
-  ins_uart_tx_axis : entity work.uart_tx_axis(rtl)
-    generic map
-    ( WIDTH           => 8
-      )
-    port map
-    ( clk_i           => clk_i
-     ,arst_b_i        => arst_b_i
-     ,s_axis_tdata_i  => s_axis_tdata
-     ,s_axis_tvalid_i => s_axis_tvalid
-     ,s_axis_tready_o => s_axis_tready
-     ,uart_tx_o       => uart_tx_o
-     ,baud_tick_i     => uart_baud_tick
-     ,parity_enable_i => '0'
-     ,parity_odd_i    => '0'
-      );
+  gen_uart_tx: if UART_TX_ENABLE = true
+  generate
 
---  ins_uart_rx_axis : entity work.uart_rx_axis(rtl)
---    generic map
---    ( WIDTH           => 8
---      )
---    port map
---    ( clk_i           => clk
---     ,arst_b_i        => arst_b_supervisor
---     ,m_axis_tdata_o  => open
---     ,m_axis_tvalid_o => open
---     ,m_axis_tready_i => '1'
---     ,uart_rx_i       => uart_tx
---     ,baud_tick_i     => uart_baud_tick
---     ,parity_enable_i => '0'
---     ,parity_odd_i    => '0'
---      );
+    ins_uart_tx_baud_rate_gen : entity work.uart_baud_rate_gen(rtl)
+      generic map(
+        BAUD_RATE      => BAUD_RATE,
+        CLOCK_FREQ     => CLOCK_FREQ
+        )
+      port map(
+        clk_i            => clk_i   ,
+        arst_b_i         => arst_b_i,
+        baud_tick_en_i   => uart_tx_baud_tick_en,
+        baud_tick_o      => uart_tx_baud_tick,
+        baud_tick_half_o => open
+        );
 
+    uart_tx_baud_tick_en <= '1';
+    s_axis_tvalid        <= ip_cs and ip_we;
+    s_axis_tdata         <= ip_wdata;
+    
+    ins_uart_tx_axis : entity work.uart_tx_axis(rtl)
+      generic map
+      ( WIDTH           => 8
+        )
+      port map
+      ( clk_i           => clk_i
+       ,arst_b_i        => arst_b_i
+       ,s_axis_tdata_i  => s_axis_tdata
+       ,s_axis_tvalid_i => s_axis_tvalid
+       ,s_axis_tready_o => s_axis_tready
+       ,uart_tx_o       => uart_tx
+       ,baud_tick_i     => uart_tx_baud_tick
+       ,parity_enable_i => parity_enable
+       ,parity_odd_i    => parity_odd   
+       );
+    
+  end generate gen_uart_tx;
+
+  gen_uart_tx_b: if UART_TX_ENABLE = false
+  generate
+
+    s_axis_tready        <= '1';
+    uart_tx              <= '1';
+    
+  end generate gen_uart_tx_b;
+
+
+  gen_uart_rx: if UART_RX_ENABLE = true
+  generate
+
+    ins_uart_rx_baud_rate_gen : entity work.uart_baud_rate_gen(rtl)
+      generic map(
+        BAUD_RATE      => BAUD_RATE,
+        CLOCK_FREQ     => CLOCK_FREQ
+        )
+      port map(
+        clk_i            => clk_i   ,
+        arst_b_i         => arst_b_i,
+        baud_tick_en_i   => uart_rx_baud_tick_en,
+        baud_tick_o      => uart_rx_baud_tick,
+        baud_tick_half_o => uart_rx_baud_tick_half
+        );
+
+    m_axis_tready <= ip_cs and ip_re;
+    uart_rx       <= uart_tx when uart_loopback = '1' else
+                     uart_rx_i;
+
+    ins_uart_rx_axis : entity work.uart_rx_axis(rtl)
+      generic map
+      ( WIDTH           => 8
+        )
+      port map
+      ( clk_i           => clk_i
+       ,arst_b_i        => arst_b_i
+       ,m_axis_tdata_o  => m_axis_tdata
+       ,m_axis_tvalid_o => m_axis_tvalid
+       ,m_axis_tready_i => m_axis_tready
+       ,uart_rx_i       => uart_rx
+       ,baud_tick_en_o  => uart_rx_baud_tick_en
+       ,baud_tick_i     => uart_rx_baud_tick
+       ,baud_tick_half_i=> uart_rx_baud_tick_half
+       ,parity_enable_i => parity_enable
+       ,parity_odd_i    => parity_odd   
+        );
+
+  end generate gen_uart_rx;
+
+  gen_uart_rx_b: if UART_RX_ENABLE = false
+  generate
+
+    m_axis_tdata    <= (others => '0');
+    m_axis_tvalid   <= '0';
+    
+  end generate gen_uart_rx_b;
   
 end architecture rtl;
--- src/uart_rx_axis.vhd
+++ src/uart_rx_axis.vhd
@@ -13,27 +13,38 @@ entity uart_rx_axis is
     clk_i           : in  std_logic;
     arst_b_i        : in  std_logic;
     uart_rx_i       : in  std_logic;
-    m_axis_tdata_o  : out std_logic_vector(7 downto 0);
+    m_axis_tdata_o  : out std_logic_vector(WIDTH-1 downto 0);
     m_axis_tvalid_o : out std_logic;
     m_axis_tready_i : in  std_logic;
-    --interrupt       : out std_logic;
     baud_tick_i     : in  std_logic;
+    baud_tick_half_i: in  std_logic;
+    baud_tick_en_o  : out std_logic;
     parity_enable_i : in  std_logic;
     parity_odd_i    : in  std_logic
   );
 end uart_rx_axis;
 
 architecture rtl of uart_rx_axis is
+  -- Define constant 
   constant WIDTH_CNT                      : natural := clog2(WIDTH+3);
   constant UART_BIT_CNT_WITHOUT_PARITY    : unsigned(WIDTH_CNT-1 downto 0) := to_unsigned(WIDTH+1,WIDTH_CNT);
   constant UART_BIT_CNT_WITH_PARITY       : unsigned(WIDTH_CNT-1 downto 0) := UART_BIT_CNT_WITHOUT_PARITY+1;
 
+  constant BIT_START                      : natural := 0;
+  constant BIT_DATA_LSB                   : natural := BIT_START+1;
+  constant BIT_DATA_MSB                   : natural := BIT_DATA_LSB+WIDTH-1;
+  constant BIT_PARITY                     : natural := BIT_DATA_MSB+1;
+  constant BIT_MSB                        : natural := WIDTH+2-1;
+  
+  type     state_t is (IDLE, ACTIVE, STOP);
+  signal   state_r                        : state_t;
+  
   -- Déclaration des registres internes
-  signal   uart_rx_data_r                 : std_logic_vector(WIDTH+2  -1 downto 0);
+  signal   uart_rx_data_r                 : std_logic_vector(BIT_MSB     downto 0); 
   signal   uart_rx_bit_cnt_r              : unsigned        (WIDTH_CNT-1 downto 0);
   signal   uart_rx_active_r               : std_logic;
-  signal   parity_bit                     : std_logic;
-
+  signal   parity_bit_r                   : std_logic;
+  
 
   function reduce_xor(input_vector : STD_LOGIC_VECTOR) return STD_LOGIC is
     variable temp_result : STD_LOGIC := '0';
@@ -44,9 +55,11 @@ architecture rtl of uart_rx_axis is
     return temp_result;
   end reduce_xor;
 
-
 begin
 
+  baud_tick_en_o <= '1' when state_r = ACTIVE else
+                    '0';
+  
   -- Logique de réception UART
   process(clk_i, arst_b_i)
   begin
@@ -54,52 +67,54 @@ begin
     then
       uart_rx_data_r    <= (others => '0');
       uart_rx_bit_cnt_r <= (others => '0');
-      uart_rx_active_r  <= '0';
       m_axis_tvalid_o   <= '0';
-      --interrupt         <= '0';
+      state_r           <= IDLE;
+      
     elsif rising_edge(clk_i)
     then
+      -- FIFO consume the character
       if m_axis_tready_i = '1'
       then
         m_axis_tvalid_o <= '0';
       end if;
-      -- Have transmission ?
-      if uart_rx_active_r = '0'
-      then
-        -- No transmission, when START Bit
-        if baud_tick_i = '1' and uart_rx_i = '0'
-        then
-          -- Détection du bit de start
-          uart_rx_active_r <= '1';
 
-          -- Update compteur depending parity to be transmit
-          if (parity_enable_i = '1')
+      case state_r is
+        when IDLE => 
+          -- No transmission, wait START Bit
+          if uart_rx_i = '0'
           then
-            uart_rx_bit_cnt_r <= UART_BIT_CNT_WITH_PARITY;
-          else
-            uart_rx_bit_cnt_r <= UART_BIT_CNT_WITHOUT_PARITY;
-          end if;     
+            state_r          <= ACTIVE;
+            
+            -- Update counter depending parity to be transmit
+            if (parity_enable_i = '1')
+            then
+              uart_rx_bit_cnt_r <= UART_BIT_CNT_WITH_PARITY;
+            else
+              uart_rx_bit_cnt_r <= UART_BIT_CNT_WITHOUT_PARITY;
+            end if;     
 
-        end if;
-      else
-        -- Transmission in progress, have tick ?
-        if baud_tick_i = '1'
-        then
-          uart_rx_data_r    <= uart_rx_i & uart_rx_data_r(9 downto 1); -- Décalage à gauche
-          uart_rx_bit_cnt_r <= uart_rx_bit_cnt_r - 1;
-
-          -- Last bit, go inactive
-          if uart_rx_bit_cnt_r = 0
-          then
-            m_axis_tdata_o  <= uart_rx_data_r(8 downto 1); -- Extraire les bits de données
-            parity_bit      <= uart_rx_data_r(9); -- Extraire le bit de parité
-            m_axis_tvalid_o <= '1';
-
-            --interrupt <= '1'; -- Générer une interruption
-            uart_rx_active_r <= '0';
           end if;
-        end if;
-      end if;
+        when ACTIVE =>
+        -- Reception in progress, have tick ?
+          if baud_tick_half_i = '1'
+          then
+            uart_rx_data_r    <= uart_rx_i & uart_rx_data_r(BIT_MSB downto 1); -- Décalage à gauche
+            uart_rx_bit_cnt_r <= uart_rx_bit_cnt_r - 1;
+
+            -- Last bit, go inactive
+            if uart_rx_bit_cnt_r = 0
+            then
+              state_r          <= STOP;
+            end if;
+          end if;
+        when STOP =>
+          state_r         <= IDLE;
+          m_axis_tdata_o  <= uart_rx_data_r(BIT_DATA_MSB downto BIT_DATA_LSB); -- Extraire les bits de données
+          parity_bit_r    <= uart_rx_data_r(BIT_PARITY); -- Extraire le bit de parité
+          m_axis_tvalid_o <= '1';
+        when others =>
+          state_r         <= IDLE;
+      end case;
     end if;
   end process;
 
